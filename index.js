a87a87a9d934842965bfac7b03fc483bbb3b85bd
Salim <doshka23@gmail.com>
2024-04-03 00:11:07 +0200

    build: connection with success to DB

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ require('dotenv').config()
 
 const appConfig = require('./src/config/app')
 const app = require('./src/app')
+const pool = require('./src/config/pool')
+const connectionOptions = require('./src/config/connection')
 
 const PORT = appConfig.app_port
-app.listen(PORT, () => {
-  console.log('App running on port', 4003)
+
+pool.connect(connectionOptions).then(() => {
+  app.listen(PORT, () => {
+    console.log('App running on port', PORT)
+  })
+}).catch((err) => {
+  console.log(err)
 })
