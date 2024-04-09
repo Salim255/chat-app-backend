9a44afba18aa9c2c90c235e8d916ef0246a6931d
Salim <salim.hassan.welleat@gmail.com>
2024-04-09 17:57:18 +0200

    build: insert message handler

--- src/models/messageModel.js
+++ src/models/messageModel.js
@@ -1,6 +1,17 @@
 const pool = require('../config/pool')
 
 class Message {
+  static async insert (data) {
+    const { rows } = await pool.query(`
+        INSERT INTO messages
+                (content, from_user_id, chat_id)
+            VALUES
+                ($1, $2, $3) RETURNING *;
+        `, [data.content, data.userId, data.chat_id])
+
+    return rows[0]
+  }
+
   static async count () {
     const { rows } = await pool.query(`
     SELECT COUNT(*) FROM messages ;
