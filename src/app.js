181ad9d322b4b1eb42296c6c18cdfc136489855c
Salim <salim.hassan.welleat@gmail.com>
2024-04-09 17:17:14 +0200

    build: create chatUsers unit test

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 const userRouter = require('../src/views/userRouter')
 const chatRouter = require('./views/chatRouter')
+const chatUserRouter = require('./views/chatUserRouter')
 module.exports = () => {
   app.use(express.json())
   app.use('/api/v1/users', userRouter)
   app.use('/api/v1/chats', chatRouter)
+  app.use('/api/v1/chat-users', chatUserRouter)
   return app
 }
