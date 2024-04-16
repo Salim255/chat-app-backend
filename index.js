6595f42794fcc24080ddffa623a6ac7c1ddb6157
Salim <doshka23@gmail.com>
2024-04-16 12:08:48 +0200

    build: handled unhandled rejection errors

--- index.js
+++ index.js
@@ -8,9 +8,17 @@ const connectionOptions = require('./src/config/connection')
 const PORT = appConfig.app_port || 4003
 
 pool.connect(connectionOptions).then(() => {
-  app().listen(PORT, () => {
-    console.log('App running on port', PORT)
+  console.log('DB connection successful!')
+})
+
+const server = app().listen(PORT, () => {
+  console.log('App running on port', PORT)
+})
+
+process.on('unhandledRejection', (err) => {
+  console.log('ERROR NAME:=>', err.name, ' ERROR MESSAGE:=>', err.message)
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...')
+  server.close(() => {
+    process.exit(1)
   })
-}).catch((err) => {
-  console.log(err)
 })
