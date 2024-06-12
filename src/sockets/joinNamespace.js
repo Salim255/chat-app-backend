7cff943115e1cdb2796a05352766e6636eb54df8
Salim <doshka23@gmail.com>
2024-06-12 16:16:33 +0200

    corrected messaging functions

--- src/sockets/joinNamespace.js
+++ src/sockets/joinNamespace.js
@@ -1,6 +1,26 @@
 exports.joinNamespace = ({ socket, namespaceSockets, namespaceEndpoint }) => {
-  namespaceSockets[namespaceEndpoint].on('connection', (socket) => {
-    console.log("Hello bien conncteded ⛑️⛑️", socket.id, namespaceEndpoint);
+  namespaceSockets[namespaceEndpoint].on('connection', (nsSocket) => {
+    nsSocket.on('typing', () => {
+      nsSocket.broadcast.emit('typingServer', {
+        socketId: nsSocket.id,
+        type: 'typing'
+      }
+      )
+    });
+
+    nsSocket.on('stopTyping', () => {
+      nsSocket.broadcast.emit('stopTypingServer', {
+        socketId: nsSocket.id,
+        type: 'stopTyping'
+      })
+    });
+
+    nsSocket.on('sentMessage', (data) => {
+      nsSocket.broadcast.emit('emittingSentMessage', {
+        socketId: nsSocket.id,
+        partnerId: data.partnerId
+      })
+    })
   })
   socket.emit('joinedNamespace', { namespace: namespaceEndpoint })
 }
