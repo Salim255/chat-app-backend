92bef7e4ca7084d213b7fe047c815e530bf37541
Salim <salim.hassan.welleat@gmail.com>
2025-01-14 23:23:19 +0100

    resizing uplaoded image

--- src/views/userRouter.js
+++ src/views/userRouter.js
@@ -2,13 +2,13 @@ const express = require('express')
 const router = express.Router()
 const authController = require('../controllers/authController')
 const userController = require('../controllers/userController')
-const uploadUserPhoto = require('../middlewares/media/multerMiddleware');
+const multerMiddleWare = require('../middlewares/media/multerMiddleware');
 
 router.post('/signup/createUser', authController.createUser)
 router.post('/signup/createAdmin', authController.createAdmin)
 router.post('/login', authController.login)
 router.get('/', authController.protect, userController.getUserByID)
-router.patch('/updateMe', uploadUserPhoto, authController.updateMe)
+router.patch('/updateMe', multerMiddleWare.uploadUserPhoto, multerMiddleWare.resizeUserPhoto, authController.updateMe)
 router.put('/:userId/disable', authController.protect, userController.disableUser)
 
 module.exports = router
