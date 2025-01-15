6fb361dbfcd827dc7a351fa3f55dbe9e3b7c7bcf
Salim <salim.hassan.welleat@gmail.com>
2025-01-15 18:53:32 +0100

    built update me request

--- src/middlewares/media/multerMiddleware.js
+++ src/middlewares/media/multerMiddleware.js
@@ -69,11 +69,3 @@ exports.resizeUserPhoto = (req, res, next) => {
 
   next();
 }
-
-exports.filterObj = (obj, ...allowedFields) => {
-  const newObj = {};
-  Object.keys(obj).forEach(el => {
-    if (allowedFields.includes(el)) newObj[el] = obj[el];
-  })
-  return newObj;
-}
